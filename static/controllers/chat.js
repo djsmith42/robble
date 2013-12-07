d9f5dd39f100062f7571c9849aee06d0e9e0a6f6
Dave Smith <dave@thesmithfam.org>
2013-12-07 00:50:52 -0500

    Force the user to enter a name

--- static/controllers/chat.js
+++ static/controllers/chat.js
@@ -9,23 +9,25 @@ controller("ChatController", ['$scope', '$timeout', '$window', '$element', funct
     var timeout;
 
     $scope.setupComplete = function() {
-        $scope.setupNeeded = false;
-        $scope.readyToChat = true;
-        localStorage.setItem('userName for room ' + roomId, $scope.userName);
-        firebase = new Firebase('https://robblebase.firebaseio-demo.com/' + roomId);
-        firebase.on('child_added', function(snapshot) {
-            var message = snapshot.val();
-            $scope.messages.push({
-                name: message.name,
-                text: message.text
+        if ($scope.userName) {
+            $scope.setupNeeded = false;
+            $scope.readyToChat = true;
+            localStorage.setItem('userName for room ' + roomId, $scope.userName);
+            firebase = new Firebase('https://robblebase.firebaseio-demo.com/' + roomId);
+            firebase.on('child_added', function(snapshot) {
+                var message = snapshot.val();
+                $scope.messages.push({
+                    name: message.name,
+                    text: message.text
+                });
+                clearTimeout(timeout);
+                timeout = setTimeout(function() {
+                    var chatList = $element.find('ul.chat-list');
+                    chatList.animate({scrollTop: chatList[0].scrollHeight});
+                });
+                $timeout(function(){});
             });
-            clearTimeout(timeout);
-            timeout = setTimeout(function() {
-                var chatList = $element.find('ul.chat-list');
-                chatList.animate({scrollTop: chatList[0].scrollHeight});
-            });
-            $timeout(function(){});
-        });
+        }
     }
 
     $scope.sendMessage = function() {
