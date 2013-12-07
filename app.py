d73cf4d46043bc201711a1857c95379cf4a0eaa3
Dave Smith <dave@thesmithfam.org>
2013-12-07 02:01:41 -0500

    Install robble at the root URL
    And kill debug mode

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import flask, shortuuid
 
 app = flask.Flask(__name__)
-app.debug = True
-
-ROOT = '/robble'
+#app.debug = True
 
 @app.route('/')
 def index():
     roomid = shortuuid.uuid()[:7]
-    return flask.redirect('{0}/{1}'.format(ROOT, roomid))
+    return flask.redirect('/{0}'.format(roomid))
 
 @app.route('/<roomid>')
 def chat(roomid):
